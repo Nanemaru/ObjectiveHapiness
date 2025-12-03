3fee62fed1815d0d608df8092857bc4f58b008f2
Nanemaru <mylanboitel@gmail.com>
2025-12-03 11:23:00 +0100

    Update character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Character : MonoBehaviour
 {
+    public string job;
+    public int year;
+    private NavMeshAgent agent;
+    private enum State {Yes,No}
+    private State Hunger = State.No;
+    private State Tired = State.No;
+
     void Start()
     {
         
