8558df53be4b1e79ffe676118823ed748995ed2b
Axel <axel.riou405@gmail.com>
2025-12-10 15:14:27 +0100

    block camera on map limit

--- Assets/Scripts/MouseNavigation.cs
+++ Assets/Scripts/MouseNavigation.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseNavigation : MonoBehaviour
@@ -8,10 +6,10 @@ public class MouseNavigation : MonoBehaviour
     void Update()
     {
         //When mouse reach a border of the screen, call MoveCamera function with the vector of the direction
-        if (Input.mousePosition.y >= Screen.height *0.95) MoveCamera(Vector3.forward);
-        if (Input.mousePosition.y <= Screen.height *0.05) MoveCamera(Vector3.back);
-        if (Input.mousePosition.x >= Screen.width *0.95) MoveCamera(Vector3.right);
-        if (Input.mousePosition.x <= Screen.width *0.05) MoveCamera(Vector3.left);
+        if (Input.mousePosition.y >= Screen.height *0.95 && transform.position.z <= -125) MoveCamera(Vector3.forward);
+        if (Input.mousePosition.y <= Screen.height *0.05 && transform.position.z >= -200) MoveCamera(Vector3.back);
+        if (Input.mousePosition.x >= Screen.width *0.95 && transform.position.x <= 487) MoveCamera(Vector3.right);
+        if (Input.mousePosition.x <= Screen.width *0.05 && transform.position.x >= 433) MoveCamera(Vector3.left);
     }
 
     private void MoveCamera(Vector3 vectorDirection) //Move camera in the direction put in parameter
