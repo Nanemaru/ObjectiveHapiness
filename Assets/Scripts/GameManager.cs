81b1fb428f38d3485961567ac21713c4f8137f3f
Axel <axel.riou405@gmail.com>
2025-12-10 15:14:09 +0100

    code optimisation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
-using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
